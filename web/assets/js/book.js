2feef302b85730303f07ce671be6340a05ad4bd5
robaho <rengels@ix.netcom.com>
2018-09-06 17:09:01 -0500

    allow multiple books being shown, round prices to 2 decimals

--- web/assets/js/book.js
+++ web/assets/js/book.js
@@ -44,10 +44,10 @@ function buildBookHtml(book) {
     var s = "<table class='book' width='100%' height='100%'>"
     s = s + "<tr><th class='bidqty'/><th class='price'/><th class='askqty'/></tr>";
     for (let ask of book.Asks.reverse()) {
-        s = s + `<tr><td class="bidqty bid"></td><td class="price ask">${ask.Price}</td><td class="askqty ask">${ask.Quantity}</td></tr>`
+        s = s + `<tr><td class="bidqty bid"></td><td class="price ask">${Number(ask.Price).toFixed(2)}</td><td class="askqty ask">${ask.Quantity}</td></tr>`
     }
     for (let bid of book.Bids) {
-        s = s + `<tr><td class="bidqty bid">${bid.Quantity}</td><td class="price bid">${bid.Price}</td><td class="askqty ask"></td></tr>`
+        s = s + `<tr><td class="bidqty bid">${bid.Quantity}</td><td class="price bid">${Number(bid.Price).toFixed(2)}</td><td class="askqty ask"></td></tr>`
     }
     s = s + "</table>"
     return s
